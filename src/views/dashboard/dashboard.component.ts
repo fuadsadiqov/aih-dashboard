fb28f94d6a944e1e7cca2b939b23fc60f3d785fa
fuadsadiqov <sadiqovfuad8@gmail.com>
2023-07-03 12:28:54 +0400

    adding query and save local storage

--- src/views/dashboard/dashboard.component.ts
+++ src/views/dashboard/dashboard.component.ts
@@ -7,14 +7,39 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  public active: number = 1 
+  public activeTime: string | any = localStorage.getItem('time') ? localStorage.getItem('time') : 'yearly' 
+  public activeOption: string | any = localStorage.getItem('query') ? localStorage.getItem('query') : 'operational'
+  public activeMonth: string | any = localStorage.getItem('month') ? localStorage.getItem('month') : 'september'
   constructor(private route: ActivatedRoute, private router: Router){
-    this.addQuery('operational')
+    console.log(this.activeOption);
+    this.addQuery(this.activeOption)
+    this.addTime(this.activeTime)
+    this.addMonth(this.activeMonth)
+    
   }
   addQuery(option: string){
+    localStorage.setItem('query', option)
+    this.activeOption = option
     this.router.navigate([], {
       relativeTo: this.route, 
       queryParams: {option: option},
+      queryParamsHandling: 'merge'
+    })
+  }
+  addTime(time: string){
+    localStorage.setItem('time', time)
+    this.activeTime = time
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {date: time},
+      queryParamsHandling: 'merge'
+    })
+  }
+  addMonth(value: string){
+    localStorage.setItem('month', value)
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {month: value},
       queryParamsHandling: 'merge'
     })
   }
