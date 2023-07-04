034b54ef6f2bc12b5f1468c68cbfc1c950000f09
fuadsadiqov <sadiqovfuad8@gmail.com>
2023-07-04 13:47:25 +0400

    adding waterfall chart"

--- src/constants/bar-chart/bar-chart.component.ts
+++ src/constants/bar-chart/bar-chart.component.ts
@@ -11,11 +11,31 @@ import { ResponseType } from 'src/interfaces/response.interface';
 })
 export class BarChartComponent implements OnChanges{
   @Input() wrapper: ResponseType[] | any = []  
-  
+  newArr: any = []
+
   ngOnChanges(changes: SimpleChanges | any): void {
     if(changes.wrapper.currentValue.length != 0){
+      for (let index = 0; index < this.wrapper.children.length; index++) {
+        let previousElement: ResponseType | any = this.wrapper.children[index - 1];
+        let element: ResponseType | any = this.wrapper.children[index];
+        let result: number[] | any
+        const value = element?.value ?? 0;
+        const isFirstElement = index === 0;
+        const isLastElement = index === this.wrapper.children.length - 1;
+        if ((value > 0 && !isFirstElement && !isLastElement) || (value < 0 && !isFirstElement && !isLastElement)) {
+          result = [(previousElement?.value ? previousElement?.value : 0), element?.value + (previousElement?.value ? previousElement?.value : 0)]
+          element.value = element?.value + (previousElement?.value ? previousElement?.value : 0)
+        }
+        if(isFirstElement){
+          result = [0, element.value]          
+        }
+        if(isLastElement){
+          result = [previousElement.value, 0]
+        }
+        this.newArr = [...this.newArr, result]
+      }
       this.barChartData.labels = this.wrapper.children.map((item: ResponseType) => item.title)
-      this.barChartData.datasets[0].data = this.wrapper.children.map((item: ResponseType) => item.value)      
+      this.barChartData.datasets[0].data = this.newArr      
       this.chart?.update()
     }
   }
@@ -44,7 +64,7 @@ export class BarChartComponent implements OnChanges{
         display: false, 
         stacked: true,
         min: 0,
-        // max: 100000,
+        max: 350000,
       }
     },
     plugins: {
@@ -53,15 +73,23 @@ export class BarChartComponent implements OnChanges{
       },
       tooltip: {
         callbacks: {
-          
+          label(tooltipItem) {
+            let value: any = tooltipItem.parsed.y
+            if(value != null){
+              value = value.toLocaleString()
+            }
+            return value
+          },
         }
       },
       datalabels: {
         anchor: 'end',
         align: 'end',
         formatter(value, context) {
-          console.log("Previous: ", context.dataset.data[context.dataIndex - 1]);
-          console.log("Current:", context.dataset.data[context.dataIndex]);
+          let result: any = (value?.[1] - value?.[0])
+          result = (result < 0 && value?.[1] !== 0) ? `(${result * -1})` : result
+          result = (result < 0 && value?.[1] === 0) ? result * -1 : result
+          return result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");          
         },
       },
     },
@@ -73,7 +101,7 @@ export class BarChartComponent implements OnChanges{
             ctx.chart.options.backgroundColor = '#456CD7' 
             return ctx.chart.options.backgroundColor = '#456CD7'
           }
-          else if(ctx.raw > 0){
+          else if(ctx.raw?.[0] < ctx.raw?.[1]){
             return ctx.chart.options.backgroundColor = '#6EB484' 
           }
           else{          
